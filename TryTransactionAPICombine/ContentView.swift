75a55c43849f5198832486169a9c0fae41d5a17b
superkookai <weerawut@live.com>
2025-03-13 16:37:13 +0700

    Initial Commit

---
+++ TryTransactionAPICombine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TryTransactionAPICombine
+//
+//  Created by Weerawut Chaiyasomboon on 13/03/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TryTransactionAPICombine/TryTransactionAPICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryTransactionAPICombineApp.swift
+//  TryTransactionAPICombine
+//
+//  Created by Weerawut Chaiyasomboon on 13/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct TryTransactionAPICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
